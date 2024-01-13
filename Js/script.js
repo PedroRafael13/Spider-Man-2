daf666d2537d0f6be3b9c727c365e41784339e74
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-01-12 22:58:36 -0400

    projeto finalizado

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,32 @@
 var swiper = new Swiper(".slide-video", {
   slidesPerView: 3,
   spaceBetween: 30,
+  speed: 800,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".s-videos .swiper-pagination",
     clickable: true,
   },
+  navigation: {
+      nextEl: ".s-videos .btn-next",
+      prevEl: ".s-videos .btn-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      paceBetween: 16,
+    },
+
+    767: {
+      slidesPerView: 2.1,
+    },
+
+    1024: {
+      slidesPerView: 3.1,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
 });
